4007e052eca812a42253db1bfb573cb63fb652de
MayKeziah <keziah.may@gmail.com>
2019-05-02 13:44:36 -0700

    Initial commit

---
+++ Workspace/DevTeam1_Calculator/src/buttonControls/Calculator.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package buttonControls;
+
+/**
+ * @author E
+ *
+ */
+public interface Calculator {
+	public double add(double a, double b);
+	public double subtract(double a, double b);
+	public double multiply(double a, double b);
+	public double divide(double a, double b);
+	public double exponent(double a, double b);
+}
