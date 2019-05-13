9cbfd04e0e925d5d3a6606cfd579acb88214b5d2
MayKeziah <keziah.may@gmail.com>
2019-05-13 11:33:57 -0700

    Adding display function to operator buttons + and -

---
+++ Workspace/DevTeam1_Calculator/src/display/CalculateClient.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package display;
+
+/**
+ * @author E
+ *
+ */
+public class CalculateClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Calculator test = new Calculator();
+		
+
+	}
+
+}
