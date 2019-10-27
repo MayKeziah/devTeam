0179a32a9f8f13a859b7c80be3e1c94b93488f57
MayKeziah <keziah.may@gmail.com>
2019-10-27 11:20:38 -0700

    auto toolchain

--- Workspace/TreeLab/src/dataStructures/BinarySearchTree.java
+++ Workspace/TreeLab/src/dataStructures/BinarySearchTree.java
@@ -6,39 +6,234 @@ package dataStructures;
 /**
  * To learn and understand Generic Binary Search Trees.
  * 
+ * Does not allow duplicates
+ * 
  * @author E
  *
  */
 public class BinarySearchTree<T> {
-	public Node<T> overallRoot;
+	private Node<T> overallRoot;
+	private final String separator = "  ";
 	
+	/**
+	 * Constructs a binary tree with values contained in an Array
+	 * 
+	 * @param list the Array used to build the list
+	 */
 	public BinarySearchTree(T[] list) {
+		overallRoot = new Node<T>(list[0]);
 		addAll(list);
-		
 	}	
-	public class Node<T>{
-		private T 		data; 
-		private Node<T> left;
-		private Node<T> right;
+	
+	/**
+	 * Inner Node class, the leaves and branches of the tree
+	 * 
+	 * @author E
+	 *
+	 */
+	public class Node<E>{
+		private E 		data; 
+		private Node<E> left;
+		private Node<E> right;
 		
-		public Node(T data){
+		public Node(E data){
 			this(data, null, null);
 		}
-		public Node(T data, Node<T> left, Node<T> right) {
-			this.data  = data;
-			this.left  = left;
-			this.right = right;
+		public Node(E data, Node<E> left, Node<E> right) {
+				this.data  = data;
+				this.left  = left;
+				this.right = right;
+		}
+		
+		public boolean hasLeft() {
+			return left != null;
+		}
+		
+		public boolean hasRight() {
+			return right != null;
 		}
 		public String toString() {
 			return data.toString();
 		}
 	}
+
+	/**
+	 * Adds the given value to the list in natural binary order
+	 * 
+	 * @param value
+	 */
+	public void add(T value) {
+		add(overallRoot, value);
+	}
 	
+	/**
+	 * Adds a given value to the list in natural binary order.
+	 * 
+	 * @param root the current node
+	 * @param value the value to add
+	 * @return the current node
+	 */
+	@SuppressWarnings("unchecked")
+	private Node<T> add(Node<T> root, T value) {
+		if (root == null) {
+			root = new Node<T>(value);
+		}else if (((Comparable<T>) root.data).compareTo(value) > 0) {
+			root.left = add(root.left, value);
+		}else if(((Comparable<T>) root.data).compareTo(value) < 0) {
+			root.right = add(root.right, value);
+		}
+		return root;
+	}
+
+	/**
+	 * Given an array, its elements are added to the tree.
+	 * 
+	 * @param list the values to add
+	 */
+	public void addAll(T[] list) {
+		for (int i = 0; i < list.length; i++) {
+			add(overallRoot, list[i]);
+		}
+	}
+	/**
+	 * Searches the list for a given value
+	 * 
+	 * @param value the value to search for
+	 * @return whether the value was found
+	 */
+	public boolean contains(T value) {
+		return contains(overallRoot, value);
+	}
+	
+	/**
+	 * Recursively searches the tree for the given value
+	 * 
+	 * @param root the current node to compare to the value
+	 * @param value the value to search for
+	 * @return whether the value was found
+	 */
+	@SuppressWarnings("unchecked")
+	private boolean contains(Node<T> root, T value) {
+		if (root == null) {
+			return false;
+		}else if (((Comparable<T>) root.data).compareTo(value) > 0){
+			return contains(root.left, value);
+		}else if (((Comparable<T>) root.data).compareTo(value) < 0){
+			return contains(root.right, value);
+		} 	return true;
+	}
+	
+	/**
+	 * Returns the number of leaves in this tree.
+	 * 
+	 * @return number of leaves
+	 */
+	public int countLeaves() {
+		return countLeaves(overallRoot);
+	}
+
+	/**
+	 * Recursively counts the number of leaves in this tree.
+	 * @param root
+	 * @return the number of leaves found
+	 */
+	private int countLeaves(Node<T> root) {
+		if (root == null) {
+			return 0;
+		}else if (!root.hasLeft() && !root.hasRight()) {
+			return 1;
+		}else {
+			return countLeaves(root.right) + countLeaves(root.left);
+		}
+	}
+
+	public int countLevels() {
+		return countLevels(overallRoot);
+	}
+
+	private int countLevels(Node<T> root) {
+		if(root == null) {
+			return 0;
+		}
+		return 1 + Math.max(countLevels(root.left), countLevels(root.right));
+	}
+	/**
+	 * Prints the contents of the tree in order
+	 */
+	public void printInorder() {
+		System.out.println("Inorder:");
+		printInorder(overallRoot);
+		System.out.println();
+	
+	}
+
+	/**
+	 * Private helper method recursively prints in order
+	 */
+	private void printInorder(Node<T> root) {
+		if(root != null) {
+			printInorder(root.left);
+			System.out.print(root + separator);
+			printInorder(root.right);
+		}
+	}
+
+	/**
+	 * Prints the contents of the tree in preorder
+	 */
+	public void printPreorder() {
+		System.out.println("Preorder:");
+		printPreorder(overallRoot);
+		System.out.println();
+
+	}
+	
+	/**
+	 * Private helper method recursively prints in preorder
+	 */
+	private void printPreorder(Node<T> root) {
+		if (root != null) {
+			System.out.print(root + separator);
+			printPreorder(root.left);
+			printPreorder(root.right);
+		}
+		
+	}
+	
+	/**
+	 * Prints the contents of the tree in preorder
+	 */
+	public void printPostorder() {
+		System.out.println("Postorder:");
+		printPostorder(overallRoot);
+		System.out.println();
+
+	}
+	
+	/**
+	 * Private helper method recursively prints in preorder
+	 */
+	private void printPostorder(Node<T> root) {
+		if (root != null) {
+			printPostorder(root.left);
+			printPostorder(root.right);
+			System.out.print(root + separator);
+
+		}
+		
+	}
+	
+	/**
+	 * Prints a visual representation of the tree turned sideways.
+	 */
 	public void printSideways() {
 		System.out.println("Sideways Tree:");
 		printSideways(overallRoot, 0);
 	}
 
+	/**
+	 * Recursively prints a visual representation of the tree turned sideways.
+	 */
 	private void printSideways(Node<T> root, int level) {
 		if(root != null) {
 			String tab = "";
--- Workspace/TreeLab/src/tests/BinarySearchClient.java
+++ Workspace/TreeLab/src/tests/BinarySearchClient.java
@@ -3,6 +3,9 @@
  */
 package tests;
 
+
+import dataStructures.BinarySearchTree;
+
 /**
  * To test my binary search tree
  * @author E
@@ -14,7 +17,34 @@ public class BinarySearchClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Integer[] list = new Integer[5];
+		for (int i = 0; i < 5; i++) {
+			list[i] = new Integer(i);
+		}
+		BinarySearchTree<Integer> test1 = new BinarySearchTree<Integer>(list);
+		test1.printSideways();
+		test1.printInorder();
+		test1.printPreorder();
+		test1.printPostorder();
+		
+		Integer[] list2 = {5, 9, 6, 1, 3, 2, 4, 7, 12, 8};
+		BinarySearchTree<Integer> test2 = new BinarySearchTree<Integer>(list2);
+		test2.printSideways();
+		test2.printInorder();
+		test2.printPreorder();
+		test2.printPostorder();
+		
+		String[] list3 = {"Kez", "Will", "Adam", "Alex", "David", "Bob", "Zenith", "Will"};
+		BinarySearchTree<String> test3 = new BinarySearchTree<String>(list3);
+		test3.printSideways();
+		test3.printInorder();
+		test3.printPreorder();
+		test3.printPostorder();
+		
+		System.out.println(test3.contains("Will"));
+		System.out.println(test3.contains("Bob"));
+		System.out.println(test3.contains(""));
+
 
 	}
 
